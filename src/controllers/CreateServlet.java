94b6b3bbed336ba043b4013c8327cd3cfa8e538e
YumiTakagi <yumi123100@gmail.com>
2022-02-27 00:34:03 +0900

    バリデーションを追加

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
+import models.validators.TaskValidator;
 import utils.DBUtil;
 
 /**
@@ -47,14 +50,28 @@ public class CreateServlet extends HttpServlet {
             m.setUpdated_at(currentTime);
 
 
-         // データベースに保存
-            em.persist(m);
-            em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "登録が完了しました。");       // ここを追記
-            em.close();
+            // バリデーションを実行してエラーがあったら新規登録のフォームに戻る
+            List<String> errors = TaskValidator.validate(m);
+            if(errors.size() > 0) {
+                em.close();
 
-            response.sendRedirect(request.getContextPath() + "/index");
+                // フォームに初期値を設定、さらにエラーメッセージを送る
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("task", m);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+                rd.forward(request, response);
+            } else {
+                // データベースに保存
+                em.persist(m);
+                em.getTransaction().commit();
+                request.getSession().setAttribute("flush", "登録が完了しました。");
+                em.close();
+
+                // indexのページにリダイレクト
+                response.sendRedirect(request.getContextPath() + "/index");
+            }
         }
     }
-
 }
---
+++ src/models/validators/TaskValidator.java
@@ -0,0 +1,31 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Task;
+
+public class TaskValidator {
+    // バリデーションを実行する
+    public static List<String> validate(Task m) {
+        List<String> errors = new ArrayList<String>();
+
+
+        String content_error = validateContent(m.getContent());
+        if(!content_error.equals("")) {
+            errors.add(content_error);
+        }
+
+        return errors;
+    }
+
+
+    // メッセージの必須入力チェック
+    private static String validateContent(String content) {
+        if(content == null || content.equals("")) {
+            return "タスクの内容を入力してください。";
+        }
+
+        return "";
+    }
+}
